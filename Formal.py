34eae4c07a2578839f558ed85fb07e1c701a0dda
David Kaplan <devik.kaplan@gmail.com>
2018-08-30 13:11:39 +0100

    Add files via upload

--- Formal.py
+++ Formal.py
@@ -3,11 +3,16 @@ import re
 import emoji
 import statistics
 import math
+import quandl
+from tkinter import *
+
 
 consumer_key = 'kgfbFJJwwp3I2gHyT1ibNVvPJ'
 consumer_secret = 'AoNjgNDvRn528ZnMG1funqEXeTZ760ZX7JGAmAgkKskrkzWNVp'
 access_token = '1017368312081154048-MQnLaHAveFccgzDlHBXyiwWBSSWSXc'
 access_token_secret = 'i2x3zhDRqgEAjyuP9puLBMLR0bLDje63rPeDIqDi1J1lY'
+
+Quandl_API = "9AK1N1LNy7PzHefyRR9w"
 
 global positiveEmojiList
 global negativeEmojiList
@@ -146,6 +151,67 @@ class Twitter(object):
             print("error")
 
 
-
 Twitter()
 
+class Stock(object):
+
+    def __init__(self):
+        quandl.ApiConfig.api_key = Quandl_API
+        self.StockData()
+
+    def StockData(self):
+        try:
+            data = quandl.get('WIKI/MSFT', rows=50)
+            data.reset_index()
+
+            closeValues = data['Close'].get_values()  # The close Value will be used in calculating coefficients
+
+        except:
+            pass
+
+
+Stock()
+
+class Window(Frame):
+
+    def __init__(self,master=None):
+        Frame.__init__(self,master)
+        self.master= master
+        self.main_window()
+
+    def main_window(self):
+
+        self.master.title("Main")
+        self.master.configure(background='snow',highlightbackground='light steel blue')
+
+        self.LabelT = Label(self.master, text="Select Company:", font=("Calibri", 12))
+        self.var = StringVar(self.master)
+
+        self.Choice = [
+            "AAPL",
+            "AMZN",
+            "MSFT"
+        ]
+        self.var.set(self.Choice[0])
+        self.w = OptionMenu(self.master, self.var, *self.Choice)  # Drop Down Menu
+        self.ButtonT = Button(self.master, text="Enter", command=self.CompanyEntry)
+
+        self.LabelT.place(x=30, y=100)
+        self.w.place(x=30, y=120)
+        self.ButtonT.place(x=30, y=170)
+
+
+    def CompanyEntry(self):
+        """Retrives Entry of USER"""
+        try:
+            x = self.var.get()
+            print(x)
+
+        except:
+            print("Error")
+
+
+root = Tk()
+root.geometry("660x440")
+app = Window(root)
+root.mainloop()
