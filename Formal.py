d3f188d4b6029dcaed191b25b6871484f00b915b
David Kaplan <devik.kaplan@gmail.com>
2018-10-19 12:21:53 +0100

    Comment added!

--- Formal.py
+++ Formal.py
@@ -23,7 +23,7 @@ from matplotlib import *
 import json
 import random
 
-#======== API Keys (Tweepy and Quandl) =========#
+#======== API Keys (Tweepy and Quandl) ========#
 
 consumer_key = 'kgfbFJJwwp3I2gHyT1ibNVvPJ'
 consumer_secret = 'AoNjgNDvRn528ZnMG1funqEXeTZ760ZX7JGAmAgkKskrkzWNVp'
@@ -93,264 +93,276 @@ negativeEmojiList = [
 class Twitter(object):
 
     def __init__(self):
-        #=====Access Twitter Database by calling on the API Keys=====#
-        self.auth = OAuthHandler(consumer_key, consumer_secret)
+        """
+        Access Twitter Database by calling on the API Keys
+        """
+        self.auth = OAuthHandler(consumer_key, consumer_secret) 
         self.auth.set_access_token(access_token_key, access_token_secret)
-        self.api = API(self.auth)
+        self.api = API(self.auth) # Used for the search method in the Main Function
 
     def Trends(self):
         global TopTopics
-        Trends = self.api.trends_place(
-            1)  # Uses the API to get most retweeted tweets in the world(1)
+        Trends = self.api.trends_place(1)  # Uses the API to get most retweeted tweets in the world(1)
         Trend_Data = Trends[0]
-        Trend_Data = Trend_Data['trends']  #Creates a data frame for Trend data
+        Trend_Data = Trend_Data['trends']  # Creates a data frame for Trend data
         topicname = [Trend_Data['name'] for Trend_Data in Trend_Data]
         TopTopics = list()
         for i in topicname[0:5]:
-            TopTopics.append(i)  #Appends the top 5 retweeted tweets.
+            TopTopics.append(i)  # Appends the top 5 retweeted tweets.
 
-        TopTopics = '\n'.join(
-            TopTopics)  #creates a line between every Top Topic
-        #TopTopics will be displayed in the tkinter page
+        TopTopics = '\n'.join(TopTopics) 
+        # Creates a line between every Top Topic
+        # TopTopics will be displayed in the tkinter page
         print(TopTopics)
 
     def Remove_URL(self, tweet):
-        #Remove URL function
-        #Removes URL from parsed_tweet and replaces them with '', (space)
-
+        """
+        Remove URL function
+        Removes URL from parsed_tweet and replaces them with '', (space)
+        """
         try:
-            noUrl = re.sub(r'http\S+', '', str(tweet.text))
-            return noUrl
+            noUrl = re.sub(r'http\S+', '', str(tweet.text)) # Uses regex module to convert URL patterns to spaces
+            return noUrl # Returns the text without URLS
 
         except:
-            print("Error")
+            print("Error") # Basic Error Handling
 
     def IdentifyEmoji(self, CleanTweet):
-        #Identify Emojis
-        #Calls on Emoji class and convert the image of emojis to description from the no url tweet data
-
+        """
+        Identify Emojis
+        Calls on Emoji class and convert the image of emojis to description from the no url tweet data
+        """
         try:
-            noEmoji = emoji.demojize(CleanTweet)
-            return noEmoji
+            noEmoji = emoji.demojize(CleanTweet) # Uses Emoji module to convert emoji image to description
+            return noEmoji # Return text without emojis
 
         except:
-            print("Error in converting emojis")
+            print("Error in converting emojis") #  Basic Error Handling
 
     def CountSentimentOfEmojis(self, CleanTweetNoEmoji):
-        #Count sentiment of Emojis by comparing emojis to the array of positive and negative emojis
-
+        """
+        Count sentiment of Emojis by comparing emojis to the array of positive and negative emojis
+        """
         global positiveCounter
         global negativeCounter
-        positiveCounter = 0
-        negativeCounter = 0
+        positiveCounter = 0 # Sets the positve counter to 0
+        negativeCounter = 0 # Sets the negative counter to 0
 
-        for word in CleanTweetNoEmoji.split():
-            if word in positiveEmojiList:
-                positiveCounter += 1
+        for word in CleanTweetNoEmoji.split(): # Splits the sentence to words
+            if word in positiveEmojiList: # Checks for emojis in the positve emoji list
+                positiveCounter += 1 # Adds 1 if emoji present in the positve emoji list
 
-            elif word in negativeEmojiList:
-                negativeCounter += 1
+            elif word in negativeEmojiList: # Checks for emojis in the negative emoji list
+                negativeCounter += 1 # Adds 1 if emoji present in the negative emoji list
 
             else:
                 pass
 
-        return positiveCounter, negativeCounter
+        return positiveCounter, negativeCounter # Return positive and negative emoji counter
 
     def ClassifyWords(self, CleanTweet):
         global posWordCounter
         global negWordCounter
 
-        posWordCounter = 0
-        negWordCounter = 0
+        posWordCounter = 0 # Sets the positive word counter to 0
+        negWordCounter = 0 # Sets the negative word counter to 0
 
-        PosFile = open("PositiveWords.txt").read()
-        NegFile = open("NegativeWords.txt").read()
+        PosFile = open("PositiveWords.txt").read() # Reads the positive word files
+        NegFile = open("NegativeWords.txt").read() # Reads the negative word file
 
-        nomarks = CleanTweet.replace('!', '')
-        nomarks = nomarks.lower()
-        nomarks = nomarks.replace('.', '')
-        nomarks = nomarks.replace(':', '')
+        nomarks = CleanTweet.replace('!', '') # Removes exclatiom marks from tweets
+        nomarks = nomarks.lower() # Converts the text to lower, so no capitals as the word list are all lower case
+        nomarks = nomarks.replace('.', '') # Replaces full stops with spaces
+        nomarks = nomarks.replace(':', '') # Replaces colons with spaces
 
-        word_counter = dict()
+        word_counter = dict() # Creates an empty dictionary
 
         try:
-            for words in nomarks.split():
+            for words in nomarks.split(): # Iterates through words in the tweet text
                 if words in word_counter:
-                    word_counter[words] += 1
+                    word_counter[words] += 1 # Adds 1 to diciontary for associted word if already present in dictionary
                 else:
-                    word_counter[words] = 1
+                    word_counter[words] = 1 # Sets the value of the key (Word) to 1.
             for words in nomarks.split():
-                if words in PosFile.splitlines():
-                    if word_counter[words] > 1:
-                        posWordCounter += (word_counter[words] * 1.3)
-                        print(word_counter[words], posWordCounter)
+                if words in PosFile.splitlines(): # Checks if a word in the text is present in the positive file
+                    if word_counter[words] > 1: # If the associated word is present in the text more than once
+                        posWordCounter += (word_counter[words] * 1.3) # sets the positive counter to equal 1.3 * the value for the associated key word
+                        print(word_counter[words], posWordCounter) 
                         print(words)
                     else:
-                        posWordCounter += 1
+                        posWordCounter += 1 # If word present in positive file but not present more than once in the tweet, then add 1 to the positive word counter
                         print(words)
-                elif words in NegFile.splitlines():
-                    if word_counter[words] > 1:
-                        negWordCounter += (word_counter[words] * 1.3)
+                elif words in NegFile.splitlines(): # Checks if a word in the text is present in the negative file
+                    if word_counter[words] > 1: # If the associated word is present in the text more than once
+                        negWordCounter += (word_counter[words] * 1.3) #sets the negative counter to equal 1.3 * the value for the associated key word
                         print(word_counter[words], negWordCounter)
                         print(words)
                     else:
-                        negWordCounter += 1
+                        negWordCounter += 1 #If word present in negative file but not present more than once in the tweet, then add 1 to the positive word counter
                         print(words)
 
         except:
-            print("Error")
+            print("Error") # Basic Error Handling
 
         return posWordCounter, negWordCounter
 
     def FrequencyTables(self, CleanTweet):
         global OverallTotal
-        positiveTweets = 0
-        negativeTweets = 0
+        positiveTweets = 0 # Sets the positve tweet counter to 0
+        negativeTweets = 0 # Sets the negative tweet counter to 0
 
-        TotalPos = posWordCounter + positiveCounter
-        TotalNeg = negWordCounter + negativeCounter
-        OverallTotal = (TotalPos - TotalNeg) / (len(CleanTweet.split()))
-        OverallTotalToPlot.append(OverallTotal)
+        TotalPos = posWordCounter + positiveCounter # Adds the positive word counter and positve emoji counter together
+        TotalNeg = negWordCounter + negativeCounter # Adds the negative word counter and negative emoji counter together
+        OverallTotal = (TotalPos - TotalNeg) / (len(CleanTweet.split())) # Subtract negative counter from positive counter and divides result
+        OverallTotalToPlot.append(OverallTotal) # Appends Results to array which would be used to plot sentiment of tweets in the main graph
 
         if OverallTotal > 0:
             print("Positive Tweet", OverallTotal)
             try:
-                positive_tweets[CleanTweet] = OverallTotal
-                print(positive_tweets)
+                positive_tweets[CleanTweet] = OverallTotal # Appends the tweet and sets the value to sentiment result
+                print(positive_tweets) # Prints out the positive value to user on the python console
             except:
-                print("error")
+                print("error") # Basic Error handling
         elif OverallTotal < 0:
             print("Negative Tweet", OverallTotal)
             try:
-                negative_tweets[CleanTweet] = OverallTotal
-                print(negative_tweets)
+                negative_tweets[CleanTweet] = OverallTotal # Append the tweet and sets the value to sentiment result
+                print(negative_tweets) # Prints out the negative value to user on the python console
 
             except:
-                print("error")
+                print("error") # Basic Error Handling
 
         else:
-            print("Neutral")
+            print("Neutral") #Prints out neutral for any other tweet that is not classified in the If statements above.
 
-        return positiveTweets, negativeTweets, OverallTotal
+        return positiveTweets, negativeTweets, OverallTotal # Return Variables
 
     def Main(self):
 
         search = Cursor(
             self.api.search, q=('#' + str(query)), lang='en', count=10)
+        # Search Method for Twitter API. Uses the user entry for the query
+        # Sets the parameters of the search method; Language = english, Count of retweets = 10
 
         global counterOfTweets
-        counterOfTweets = 0
-        TotalPosTweets = 0
-        TotalNegTweets = 0
-        TotalNeuTweets = 0
+        counterOfTweets = 0 # Sets the counter of tweets to 0
+        TotalPosTweets = 0 # Sets the positive tweet counter to 0
+        TotalNegTweets = 0 # Sets the neutral tweets counter to 0
+        TotalNeuTweets = 0 # Sets the negatuve tweets counter to 0
 
         try:
-            for tweet in search.items(20):
-
-                counterOfTweets += 1
-                CleanTweet = self.Remove_URL(tweet)
-                CleanTweetNoEmoji = self.IdentifyEmoji(CleanTweet)
-                CountEmoji = self.CountSentimentOfEmojis(CleanTweetNoEmoji)
-                classifyWords = self.ClassifyWords(CleanTweet)
-                countNumbers = self.FrequencyTables(CleanTweet)
+            for tweet in search.items(20): #Iterates through 20 pulled tweets
+                counterOfTweets += 1 # Adds 1 to the parsed tweets
+                CleanTweet = self.Remove_URL(tweet) # Calls on Remove URL function
+                CleanTweetNoEmoji = self.IdentifyEmoji(CleanTweet) # Calls on Identify Emoji function
+                CountEmoji = self.CountSentimentOfEmojis(CleanTweetNoEmoji) # Calls on classification of emojis function
+                classifyWords = self.ClassifyWords(CleanTweet) # Calls on classify word function
+                countNumbers = self.FrequencyTables(CleanTweet) # Calls on Sentiment calculator
 
                 print("noEmoji:", counterOfTweets, CleanTweet,
                       CleanTweetNoEmoji, CountEmoji, classifyWords,
                       countNumbers)
-                if OverallTotal > 0:
-                    TotalPosTweets += 1
+                if OverallTotal > 0: #  If the Overall value of sentiment is bigger than 0
+                    TotalPosTweets += 1 # Add 1 to the positve tweet counter
 
-                elif OverallTotal < 0:
-                    TotalNegTweets += 1
+                elif OverallTotal < 0: #If Overall value of sentiment is bigger than 0
+                    TotalNegTweets += 1 # Add 1 to the negative tweet counter
 
                 else:
-                    TotalNeuTweets += 1
+                    TotalNeuTweets += 1 # Add 1 to neutral tweet counter
 
             print(TotalPosTweets, TotalNegTweets)
 
             global OverallSentiment
             OverallSentiment = ((TotalPosTweets - TotalNegTweets) / (
                 TotalPosTweets + TotalNegTweets + TotalNeuTweets))
+            # Overall Sentiment Shows the overall sentiment of all tweets
+            # Calculated by subtracting negaitve tweets from positive tweets and dividing the result by all the parsed tweets
             print("Overall the Total Sentiment of", str(counterOfTweets),
                   "tweets is:", OverallSentiment)
-
+            # Print statement showing the overall sentiment of CounterOfTweets(20) and the overall sentiment of those tweets
             if OverallSentiment > 0:
-                print("Hence , Positive")
+                print("Hence , Positive") # If the pverall sentiment is bigger than 0, then overall sentiment of 20 parsed tweets is positive
             elif OverallSentiment < 0:
-                print("Hence, Negative")
+                print("Hence, Negative") # If the overall sentiment is smaller than 0, then overall sentiment of 20 parsed tweets is negative
             else:
-                print("Hence, Neutral")
+                print("Hence, Neutral") # Prints Neutral if overall sentiment not classified in the IF statments above.
 
         except error.TweepError as e:
-            print(e.reason)
-            print("error")
+            print(e.reason) # Prints Error of Twitter Search method to the user
+            print("error") # Basic Error Handling
 
 
 class Stock(object):
 
     def __init__(self):
+        """
+        Access the Quandl Databse via API keys
+        """
         quandl.ApiConfig.api_key = Quandl_API
         self.Twitter = Twitter()
-        self.StockData()
+        self.StockData() # Calls on StockData Function
 
     def StockData(self):
         """
             Pulls Stock Data via the Quandl API
         """
         try:
-            print(stockentry)
+            print(stockentry) # Print Statement showing user entry; done for compeltion purposes
             global Data
-            #data = quandl.get('WIKI/' + str(stockentry), rows=50)
-            data = quandl.get('EOD/' + str(stockentry), rows=7)
+            data = quandl.get('EOD/' + str(stockentry), rows=7) # Retrieves the last 7 days end of day prices for the user selected company
             print(data)
             Data = data
 
             return Data
 
         except:
-            pass
+            pass # Basic Error Handling
 
 
 class Window(Frame):
 
     def __init__(self, master=None):
-        Frame.__init__(self, master)
+        Frame.__init__(self, master) # initiate the Frame of the window
         self.master = master
         self.Stock = Stock()
         self.Twitter = Twitter()
         self.main_window()
 
     def main_window(self):
-        self.master.title("Main")
+        self.master.title("Main") # Sets the title of the page as main.
         self.master.configure(
-            background='snow', highlightbackground='light steel blue')
+            background='snow', highlightbackground='light steel blue') # Sets the background color of the page
 
-        self.Twitter.Trends()
+        self.Twitter.Trends() # Calls on the twitter trend function by referral statment
 
-        self.canvas = Canvas(self.master, width=670, height=450)
-        self.canvas.grid(row=0, column=0, sticky='nsew')
+        self.canvas = Canvas(self.master, width=670, height=450) # Initiate the canvas size
+        self.canvas.grid(row=0, column=0, sticky='nsew') # Places the canvas at row 0 and column 0
 
         self.line = self.canvas.create_rectangle(
-            329, -10, 332, 450, fill='light blue'
-        )  #Seprates the Main window - Left=Stock Right=Twitter Query
+            329, -10, 332, 450, fill='light blue')  
+        #Seprates the Main window - Left=Stock Right=Twitter Query
 
         self.rectangle = self.canvas.create_rectangle(
             0, 0, 660, 45, fill='light sky blue', outline='light sky blue')
+        # Creates a rectangle and fills the rectangle with a light sky blue color. This will be used as a banner for the title of the page
         self.rectangle_bottomn = self.canvas.create_rectangle(
             0, 405, 660, 440, fill='salmon', outline='salmon')
-
+        # Creates a rectangle and fills the rectange with a salmon color. 
         self.LabelTQ = Label(
             self.master, text='Twitter Query:', font=("Avenir", 14))
+        # Creates a text label. Created for easier navigation for the user
         self.EntryTQ = Entry(self.master, borderwidth=2)
-
+        # Creates an entry box where the user can enter a query
         self.ButtonTQ = Button(
             self.master,
             text="Enter",
             font=("Avenir", 14),
             command=self.TwitterQueryEntry)
-
+        # Creates a button to retrive user entry. Calls on function Twitter Query Entry, which retrives user entry
         self.LabelT = Label(
             self.master, text="Select Company:", font=("Avenir", 14))
+        # Creates a text label. Created for easier navigation for the user
         self.var = StringVar(self.master)
 
         self.Choice = ["AAPL", "MCD", "MSFT", "NKE", "INTC", "BA", "DIS"]
